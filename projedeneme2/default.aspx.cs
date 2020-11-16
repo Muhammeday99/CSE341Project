e1fb18b0ca3d36f7896a5444ee72fea9c56bbb3f
Ilkan Mert Okul <ilkanmertokul@gmail.com>
2020-11-16 18:23:32 +0300

    .

--- projedeneme2/default.aspx.cs
+++ projedeneme2/default.aspx.cs
@@ -12,7 +12,7 @@ namespace projedeneme2
     public partial class Default : System.Web.UI.Page
     {
         //This should be there so we dont need to initialize again and again.
-        SqlConnection con = new SqlConnection(@"Data source=DESKTOP-H4PMKBB;Initial Catalog=PROJECT_TRACKING_SYSTEM;Integrated Security=True");
+        SqlConnection con = databaseConnect.connectToSQL();
         protected void Page_Load(object sender, EventArgs e)
         {
             
@@ -25,9 +25,9 @@ namespace projedeneme2
             string userName = txtAd.Text;
             string userPassword = txtSifre.Text;
 
-            SqlCommand sorgula = new SqlCommand("SELECT * FROM Users WHERE Username=@Username AND Password=@Password", con);
+            SqlCommand sorgula = new SqlCommand("SELECT * FROM Users WHERE UserEmail=@UserEmail AND Password=@Password", con);
 
-            sorgula.Parameters.AddWithValue("@Username", userName);
+            sorgula.Parameters.AddWithValue("@UserEmail", userName);
             sorgula.Parameters.AddWithValue("@Password", userPassword);
             SqlDataReader oku = sorgula.ExecuteReader();
             if (oku.Read())
