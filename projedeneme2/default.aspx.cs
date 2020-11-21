c149639fedad7cec055fb6104d59d69e496b971d
mertbesirli <mert17_can@outlook.com>
2020-11-21 14:21:15 +0300

    Status'e göre admin ve users ayrıldı, gideceği sayafalar ayarlandı..

--- projedeneme2/default.aspx.cs
+++ projedeneme2/default.aspx.cs
@@ -29,33 +29,80 @@ namespace projedeneme2
 
         protected void LoginButton_Click(object sender, EventArgs e)
         {
+           
+            bool isLoggedIn = false;
+            string userEmail = string.Empty;
+            string userPassword = string.Empty;
+                  
+            userEmail = InputEmail.Text;
+            userPassword = InputPassword.Text;
+            isLoggedIn = IsValidLogin(userEmail, userPassword);
+            if (isLoggedIn)
+            {
+                Session.Add("email", userEmail);
+                Boolean checkUserType = checkUser(userEmail,userPassword);
+
+                if (checkUserType)
+                {
+                    Response.Redirect("AdminPage/BaseAdminPage.aspx");
+                }
+                else
+                {
+                    Response.Redirect("Homepage/Homepage.aspx");
+                }
+            }
+            else
+            {
+                statusLabel.Text = "Email yada şifre hatalı!";
+            }
+        }
+        //Checks if you are logged in, email and password are correct
+        private bool IsValidLogin(string userEmail, string userPassword)
+        {
+  
+            SqlCommand check = new SqlCommand("SELECT * FROM Users WHERE UserEmail=@userEmail AND Password=@userPassword", con);
+            check.Parameters.AddWithValue("@userEmail", userEmail);
+            check.Parameters.AddWithValue("@userPassword", userPassword);
+            //Opens the database.
+            con.Open();
+            SqlDataReader sqlRead = check.ExecuteReader();
+
+            if (sqlRead.Read())
+            {
+                con.Close();
+                sqlRead.Close();
+                return true;
+            }
+          
+            con.Close();
+            sqlRead.Close();
+            return false;
+           
+        }
+        //Checking whether the logged in person is an admin or a user
+        protected Boolean checkUser(string userEmail,string userPassword)
+        {
             //Opens the database.
             con.Open();
 
-            string userEmail = InputEmail.Text;
-            string userPassword = InputPassword.Text;
+            string query = "SELECT Status FROM Users WHERE [Status] = 1 and [UserEmail] = @userEmail AND [Password] = @userPassword";
 
-            //IF because user can enter nothing to textboxes.
-            if(!(userEmail.Length < 1 || userPassword.Length < 1)){
+            DataTable dt = new DataTable();
+            SqlCommand command = new SqlCommand(query, con);
+            command.Parameters.AddWithValue("@userEmail", userEmail);
+            command.Parameters.AddWithValue("@userPassword", userPassword);
 
-                SqlCommand check = new SqlCommand("SELECT * FROM Users WHERE UserEmail=@UserEmail AND Password=@Password", con);
-
-                check.Parameters.AddWithValue("@UserEmail", userEmail);
-                check.Parameters.AddWithValue("@Password", userPassword);
-                SqlDataReader sqlRead = check.ExecuteReader();
-
-                if (sqlRead.Read())
-                    Response.Redirect("Homepage/Homepage.aspx");
-                else
-                    statusLabel.Text = "Email yada şifre hatalı!";
-
-                sqlRead.Close();
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                adapter.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    return true;
+                }
             }
-            else
-                statusLabel.Text = "Lütfen Email ve şifreyi giriniz!";
-            
             con.Close();
-            con.Dispose();
+
+            return false;
         }
     }
 }
