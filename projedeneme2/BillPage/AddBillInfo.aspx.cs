c2fd47c8fc8f8ba1cd39e5638d56787706114122
Muhammet Fikret ATAR <f.atar2018@gtu.edu.tr>
2020-11-30 19:55:46 +0300

    AddBillInfo Page oluşturuldu..

---
+++ projedeneme2/BillPage/AddBillInfo.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace projedeneme2.BillPage
+{
+    public partial class AddBillInfo : System.Web.UI.Page
+    {
+
+        protected SqlConnection con = databaseConnect.connectToSQL();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand check = new SqlCommand("SELECT [entityCode] FROM [dbo].[Entity_Card]");
+            check.CommandType = System.Data.CommandType.Text;
+            check.Connection = con;
+
+            string temp = "";
+            SqlDataReader reader = check.ExecuteReader();
+
+            while (reader.Read())
+            {
+                temp = reader["entityCode"].ToString();
+            }
+
+            entity.Text = temp;
+        }
+
+        protected void saveButtonClick(object sender, EventArgs e)
+        {
+            con.Open();
+
+            string BillNo = BillNoBox.Text;
+            
+            string BillDate = DateTime.Now.ToString(BillDateBox.Text);
+            //string entityCode = entityCodeBox.Text;
+            string BillDescription = BillDescriptionBox.Text;
+            string PaymentDate = DateTime.Now.ToString(PaymentDateBox.Text);
+            string KDV = Convert.ToString(KDVBox.Text);
+            string Amount = Convert.ToString(AmountBox.Text) ;
+            string EntityCode = string.Empty;
+           
+           
+
+
+            String q = "INSERT INTO dbo.Bill_info (BillNo, BillDate,BillDescription,PaymentDate,KDV,Amount) VALUES (@BillNo, @BillDate, @BillDescription, @PaymentDate, @KDV, @Amount)";
+
+            SqlCommand cmnd = new SqlCommand(q, con);
+
+            cmnd.Parameters.AddWithValue("@BillNo", BillNo);
+            cmnd.Parameters.AddWithValue("@BillDate", BillDate);
+            //cmnd.Parameters.AddWithValue("@entityCode", entityCode);
+            cmnd.Parameters.AddWithValue("@BillDescription", BillDescription);
+            cmnd.Parameters.AddWithValue("@PaymentDate", PaymentDate);
+            cmnd.Parameters.AddWithValue("@KDV", KDV);
+            cmnd.Parameters.AddWithValue("@Amount", Amount);
+
+            cmnd.ExecuteNonQuery();
+           
+        }
+    }
+}
---
+++ projedeneme2/BillPage/AddBillInfo.aspx.designer.cs
@@ -0,0 +1,107 @@
+﻿//------------------------------------------------------------------------------
+// <otomatik üretildi>
+//     Bu kod bir araç tarafından oluşturuldu.
+//
+//     Bu dosyada yapılacak değişiklikler yanlış davranışa neden olabilir ve
+//     kod tekrar üretildi. 
+// </otomatik üretildi>
+//------------------------------------------------------------------------------
+
+namespace projedeneme2.BillPage
+{
+
+
+    public partial class AddBillInfo
+    {
+
+        /// <summary>
+        /// form1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// BillNoBox denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox BillNoBox;
+
+        /// <summary>
+        /// BillDateBox denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox BillDateBox;
+
+        /// <summary>
+        /// entity denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label entity;
+
+        /// <summary>
+        /// BillDescriptionBox denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox BillDescriptionBox;
+
+        /// <summary>
+        /// PaymentDateBox denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox PaymentDateBox;
+
+        /// <summary>
+        /// KDVBox denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox KDVBox;
+
+        /// <summary>
+        /// AmountBox denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox AmountBox;
+
+        /// <summary>
+        /// saveAccount denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button saveAccount;
+
+        /// <summary>
+        /// errorLabel denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label errorLabel;
+    }
+}
