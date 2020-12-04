35407dae1fdb6c5654dc4d5970337d3a5d5b4d32
Muhammeday99 <mohammed.ay99@gmail.com>
2020-12-04 21:49:17 +0300

    added a script folder for JS files

---
+++ projedeneme2/Scripts/listCards.js
@@ -0,0 +1,14 @@
+﻿var tablebody = document.getElementById("dataTable").getElementsByTagName("tbody")[0];
+
+let tRow = tablebody.insertRow();
+
+let newcell = tRow.insertCell();
+
+let text = document.createTextNode("Hello");
+
+newcell.appendChild(text);
+
+newcell = tRow.insertCell();
+text = document.createTextNode("There");
+
+newcell.appendChild(text);
