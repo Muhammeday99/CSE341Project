20b16ae0809bcc064b35037259b89829e4cb1188
Ilkan Mert Okul <ilkanmertokul@gmail.com>
2020-12-14 21:55:52 +0300

    yeni sayfa - şuanlık işlevsiz

---
+++ projedeneme2/ProjeTanim/ProjeTanım.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ProjeTanım.aspx.cs" Inherits="projedeneme2.ProjeTanim.ProjeTanım" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            PROJE TANIM:<br />
+            <br />
+            Proje Adı :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="projectName" runat="server"></asp:TextBox>
+            <br />
+            Proje Kodu:&nbsp;&nbsp;&nbsp; <asp:TextBox ID="projectCode" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Müşteri Kodu:&nbsp;&nbsp;&nbsp; <asp:TextBox ID="clientCode" runat="server"></asp:TextBox>
+            <br />
+            Sipariş Kodu:&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="orderCode" runat="server"></asp:TextBox>
+            <br />
+            Arge Fatura Numarası:<asp:TextBox ID="billCode" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Başlangıç TarBaşlangıç Tarihi:<asp:TextBox runat="server" TextMode="Date" ID="startDate" ></asp:TextBox></asp:TextBox>
+            <br />
+            Bitiş Tarihi:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox runat="server" TextMode="Date" ID="finishDate" ></asp:TextBox>
+            <br />
+            <br />
+            Döviz Kodu:&nbsp;&nbsp;&nbsp;<asp:TextBox ID="exchangeCode" runat="server"></asp:TextBox>
+            <br />
+            Bedel:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="feeBox" runat="server"></asp:TextBox>
+            <br />
+            KDV:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="KDVbox" runat="server"></asp:TextBox>
+            <br />
+            İşçilik Ücreti:&nbsp;&nbsp;
+            <asp:TextBox ID="workFee" runat="server"></asp:TextBox>
+            <br />
+            Toplam Tutar: <asp:Label ID="totalMoneyLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            Proje Yetkilisi:<asp:TextBox ID="authorizedBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+&nbsp;<asp:Ch&nbsp;<asp:CheckBox ID="activeProject" runat="server" Text="Aktif Proje" TextAlign="Left" />
+            <br />
+            <br />
+            <asp:Button ID="saveButton" runat="server" Text="Button" OnClick="saveButton_Click" />
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ projedeneme2/ProjeTanim/ProjeTanım.aspx.designer.cs
@@ -0,0 +1,161 @@
+﻿//------------------------------------------------------------------------------
+// <otomatik üretildi>
+//     Bu kod bir araç tarafından oluşturuldu.
+//
+//     Bu dosyada yapılacak değişiklikler yanlış davranışa neden olabilir ve
+//     kod tekrar üretildi. 
+// </otomatik üretildi>
+//------------------------------------------------------------------------------
+
+namespace projedeneme2.ProjeTanim
+{
+
+
+    public partial class ProjeTanım
+    {
+
+        /// <summary>
+        /// form1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// projectName denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox projectName;
+
+        /// <summary>
+        /// projectCode denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox projectCode;
+
+        /// <summary>
+        /// clientCode denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox clientCode;
+
+        /// <summary>
+        /// orderCode denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox orderCode;
+
+        /// <summary>
+        /// billCode denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox billCode;
+
+        /// <summary>
+        /// startDate denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox startDate;
+
+        /// <summary>
+        /// finishDate denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox finishDate;
+
+        /// <summary>
+        /// exchangeCode denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox exchangeCode;
+
+        /// <summary>
+        /// feeBox denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox feeBox;
+
+        /// <summary>
+        /// KDVbox denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox KDVbox;
+
+        /// <summary>
+        /// workFee denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox workFee;
+
+        /// <summary>
+        /// totalMoneyLabel denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label totalMoneyLabel;
+
+        /// <summary>
+        /// authorizedBox denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox authorizedBox;
+
+        /// <summary>
+        /// activeProject denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox activeProject;
+
+        /// <summary>
+        /// saveButton denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button saveButton;
+    }
+}
