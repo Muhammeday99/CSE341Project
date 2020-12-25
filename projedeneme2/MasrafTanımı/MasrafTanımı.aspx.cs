d4807f2bb60841db7904d4562ed86ac51afd753b
mertbesirli <mert17_can@outlook.com>
2020-12-25 13:34:48 +0300

    Masraf Tanımı Ekle ve Sil eklendi, Sil kısmı comment halinde şimdilik

---
+++ projedeneme2/MasrafTanımı/MasrafTanımı.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace projedeneme2.MasrafTanımı
+{
+    public partial class MasrafTanımı : System.Web.UI.Page
+    {
+        protected SqlConnection con = databaseConnect.connectToSQL();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SaveButton_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string expenseCode = ExpenseCode.Text;
+            string expenseName = ExpenseName.Text;
+            string expenseDate = DateTime.Now.ToString(ExpenseDate.Text);
+
+            String q = "INSERT INTO dbo.Expense_Info (ExpenseCode,ExpenseName,ExpenseDate) VALUES (@ExpenseCode, @ExpenseName, @ExpenseDate)";
+            SqlCommand cmnd = new SqlCommand(q, con);
+
+            cmnd.Parameters.AddWithValue("@ExpenseCode", expenseCode);
+            cmnd.Parameters.AddWithValue("@expenseName", expenseName);
+            cmnd.Parameters.AddWithValue("@expenseDate", expenseDate);
+
+            cmnd.ExecuteNonQuery();
+
+            con.Close();
+        }
+
+        protected void DeleteButton_Click(object sender, EventArgs e)
+        {
+            /*Expense Code girerek silme işlemi yapıyor, şimdilik burda kalsın
+            SqlCommand cmd = new SqlCommand(@"DELETE FROM [dbo].[Expense_Info] WHERE [ExpenseCode] ='" + ExpenseCode.Text + "'", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();*/
+        }
+    }
+}
---
+++ projedeneme2/MasrafTanımı/MasrafTanımı.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <otomatik üretildi>
+//     Bu kod bir araç tarafından oluşturuldu.
+//
+//     Bu dosyada yapılacak değişiklikler yanlış davranışa neden olabilir ve
+//     kod tekrar üretildi. 
+// </otomatik üretildi>
+//------------------------------------------------------------------------------
+
+namespace projedeneme2.MasrafTanımı
+{
+
+
+    public partial class MasrafTanımı
+    {
+
+        /// <summary>
+        /// form1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// ExpenseCode denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox ExpenseCode;
+
+        /// <summary>
+        /// ExpenseName denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox ExpenseName;
+
+        /// <summary>
+        /// ExpenseDate denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox ExpenseDate;
+
+        /// <summary>
+        /// SaveButton denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button SaveButton;
+
+        /// <summary>
+        /// DeleteButton denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button DeleteButton;
+    }
+}
