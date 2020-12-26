ab155bcc7f2f4f215b7f6d145de3bb75cf93f982
Muhammed Salih YILMAZ <msy-2000@hotmail.com>
2020-12-26 17:14:24 +0300

    Soldaki Navigasyon kısmına Masraf Tanım bağlandı.

--- projedeneme2/Homepage/Homepage.aspx.cs
+++ projedeneme2/Homepage/Homepage.aspx.cs
@@ -44,7 +44,7 @@ namespace projedeneme2.Homepage
         }
         protected void Masraf_Tanimi_click(object sender, EventArgs e)
         {
-            Response.Redirect("../Homepage/Homepage.aspx");
+            Response.Redirect("../MasrafTanim/MasrafTanim.aspx");
         }
         protected void Fatura_Giris_click(object sender, EventArgs e)
         {
