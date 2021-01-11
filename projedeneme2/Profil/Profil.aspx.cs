758e1d7b9bb9006b3f882b1dd822574ec75e7d3e
Muhammed Salih YILMAZ <msy-2000@hotmail.com>
2021-01-11 13:36:32 +0300

    Profil sayfası eklendi. Yeni Kullanıcı ekleme sayfası eklendi. Hepsine Profil sayfasından ulaşılıyor. Backend'in yenilerine taşınması lazım.

---
+++ projedeneme2/Profil/Profil.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace projedeneme2.Profil
+{
+    public partial class Profil : projedeneme2.Homepage.Homepage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void listUserButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("../YeniKullanici/YeniKullanici.aspx");
+        }
+        protected void newUserButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("../YeniKullanici/YeniKullanici.aspx");
+        }
+        
+    }
+}
